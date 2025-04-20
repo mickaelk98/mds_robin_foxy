d8b2dcf114516bdbfbb80f100a8f0e3014b84a27
mickael keita <keitamickael@gmail.com>
2025-04-20 20:28:48 +0200

    ajout d'une section de presentation de nos missions

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
         </nav>
       </header>
       <main className="w-full max-w-7xl mx-auto px-2 lg:px-0 mt-20">
-        <section className="flex flex-col gap-5 md:flex-row md:items-center">
+        <section className="flex flex-col gap-5 md:flex-row md:items-center mb-20">
           <div>
-            <h1 className="text-3xl lg:text-5xl mb-5">
+            <h1 className="text-4xl lg:text-6xl mb-5">
               Proteger vos enfants sur internet commence ici
             </h1>
-            <p className="">
+            <p className="text-xl lg:text-2xl">
               un livre essentiel pour sensibiliser les enfants et des guide pour
               aider les parents et enseigants face aux danger du numérique{" "}
             </p>
@@ -32,6 +32,41 @@ export default function Home() {
               height={100}
               className="w-full"
             />
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl lg:text-6xl mb-20 mt-5">Notre mission</h2>
+          <div>
+            <ul className="flex flex-col items-center gap-5 md:flex-row">
+              <li className="flex flex-col items-center gap-2 mb-5">
+                <Image src="/enfant.png" alt="Logo" width={100} height={100} />
+                <p className="font-bold">Pour les enfants</p>
+                <p>
+                  Foxy et Robin aide les enfants à comprendre Internet et à s’y
+                  aventurer en toute sécurité. À travers des histoires et des
+                  jeux, ils apprennent à reconnaître les dangers et à adopter
+                  les bons réflexes.
+                </p>
+              </li>
+              <li className="flex flex-col items-center gap-2 mb-5">
+                <Image src="/parent.png" alt="Logo" width={100} height={100} />
+                <p className="font-bold">Pour les parents</p>
+                <p>
+                  Nous guidons les parents dans l’accompagnement numérique de
+                  leurs enfants grâce à des conseils simples et des guides
+                  pratiques. Notre mission : créer un cadre rassurant à la
+                  maison.
+                </p>
+              </li>
+              <li className="flex flex-col items-center gap-2 mb-5">
+                <Image src="/pro.png" alt="Logo" width={100} height={100} />
+                <p className="font-bold">Pour les professionnels</p>
+                Nous fournissons des ressources pédagogiques pour sensibiliser
+                les enfants aux usages d’Internet. Nous aidons les
+                professionnels à intégrer l’éducation numérique dans leurs
+                pratiques.
+              </li>
+            </ul>
           </div>
         </section>
       </main>
