3792757ebb236be4307deae9bdb3ccc456c1c450
mickael keita <keitamickael@gmail.com>
2025-05-21 07:41:27 +0200

    modification du header

--- app/components/desktop-header.tsx
+++ app/components/desktop-header.tsx
@@ -1,44 +1,38 @@
 import Image from "next/image";
 import logo from "@/app/assets/logo.png";
-import { File } from "@/app/components/header-items/file";
-import { Forums } from "@/app/components/header-items/forums";
-import { Faq } from "@/app/components/header-items/faq";
-import { Login } from "@/app/components/header-items/login";
-import { Signup } from "@/app/components/header-items/signup";
-import { Shops } from "@/app/components/header-items/shops";
+// import { File } from "@/app/components/header-items/file";
+// import { Forums } from "@/app/components/header-items/forums";
+// import { Faq } from "@/app/components/header-items/faq";
+// import { Login } from "@/app/components/header-items/login";
+// import { Signup } from "@/app/components/header-items/signup";
+// import { Shops } from "@/app/components/header-items/shops";
 
 export function DesktopHeader() {
   return (
-    <header className="hidden lg:flex bg-[var(--green-200)]">
-      <ul className="flex items-center jusify-center w-full">
-        <li>
-          <File />
-        </li>
-        <li>
-          <Forums />
-        </li>
-        <li>
-          <Faq />
-        </li>
-        <li>
-          <Image
-            src={logo}
-            alt="Logo"
-            className="translate-x-[-70px]"
-            width={180}
-            height={105}
-          />
-        </li>
-        <li>
-          <Login />
-        </li>
-        <li>
-          <Signup />
-        </li>
-        <li>
-          <Shops />
-        </li>
-      </ul>
+    <header className="w-full bg-[var(--green-200)] py-4">
+      <div className="hidden w-full max-w-7xl mx-auto px-2 lg:px-0 lg:flex lg:items-center lg:justify-between">
+        <Image src={logo} alt="logo" className="w-[200px] h-[100px]" />
+        <ul className="flex gap-10 text-xl uppercase">
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Fiches
+          </li>
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Forum
+          </li>
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Faq
+          </li>
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Connexion
+          </li>
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Inscription
+          </li>
+          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            Boutiques
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
