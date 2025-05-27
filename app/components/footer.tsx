2883f7f7354d6cfff2e183cb2b6e3a7c279a4959
mickael keita <keitamickael@gmail.com>
2025-05-27 12:45:07 +0200

    modification du footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,6 +3,7 @@ import logo from "@/app/assets/logo.png";
 import instagram from "@/app/assets/instagram.png";
 import facebook from "@/app/assets/facebook.png";
 import { Chewy } from "next/font/google";
+import { FooterSvg } from "@/app/components/wave/footer-svg";
 
 const chewy = Chewy({
   subsets: ["latin"],
@@ -11,73 +12,78 @@ const chewy = Chewy({
 
 export function Footer() {
   return (
-    <footer className="w-full max-w-7xl mx-auto px-2 lg:px-0 translate-y-[-100px] mt-50">
-      <div className="w-full flex flex-col lg:flex-row mdjustify-between px-4">
-        <Image
-          src={logo}
-          alt="logo"
-          className="mx-auto mb-10 md:w-[200px] md:h-[100px]"
-        />
-        <div className="md:w-[98%] lg:flex md:justify-center lg:gap-30">
-          {/* debut contact */}
-          <div className="mb-20">
-            <h3
-              className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
-            >
-              contact
-            </h3>
-            <ul className="text-center">
-              <li>foxyetrobin@gmail.com</li>
-            </ul>
-          </div>
-          {/* fin contact */}
+    <>
+      <FooterSvg />
+      <footer className="bg-[var(--background)]">
+        <div className="w-full max-w-7xl mx-auto px-2 lg:px-0 lg:py-10">
+          <div className="w-full flex flex-col lg:flex-row mdjustify-between px-4">
+            <Image
+              src={logo}
+              alt="logo"
+              className="mx-auto mb-10 md:w-[200px] md:h-[100px]"
+            />
+            <div className="md:w-[98%] lg:flex md:justify-center lg:gap-30">
+              {/* debut contact */}
+              <div className="mb-20 xl:mb-0">
+                <h3
+                  className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
+                >
+                  contact
+                </h3>
+                <ul className="text-center">
+                  <li>foxyetrobin@gmail.com</li>
+                </ul>
+              </div>
+              {/* fin contact */}
 
-          {/* debut reseaux */}
-          <div className="mb-20">
-            <h3
-              className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
-            >
-              Réseaux
-            </h3>
-            <ul className="flex gap-5 md:flex-col items-center justify-center">
-              <li className="flex flex-col md:flex-row items-center gap-1">
-                <Image
-                  src={instagram}
-                  alt="instagram"
-                  className="cursor-pointer"
-                />
-                <span>@foxy_robin_</span>
-              </li>
-              <li className="flex flex-col md:flex-row items-center  gap-2">
-                <Image
-                  src={facebook}
-                  alt="facebook"
-                  className="cursor-pointer"
-                />
-                <span>@Roxy Robin</span>
-              </li>
-            </ul>
-          </div>
-          {/* fin reseaux */}
+              {/* debut reseaux */}
+              <div className="mb-20 xl:mb-0">
+                <h3
+                  className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
+                >
+                  Réseaux
+                </h3>
+                <ul className="flex gap-5 md:flex-col items-center justify-center">
+                  <li className="flex flex-col md:flex-row items-center gap-1">
+                    <Image
+                      src={instagram}
+                      alt="instagram"
+                      className="cursor-pointer"
+                    />
+                    <span>@foxy_robin_</span>
+                  </li>
+                  <li className="flex flex-col md:flex-row items-center  gap-2">
+                    <Image
+                      src={facebook}
+                      alt="facebook"
+                      className="cursor-pointer"
+                    />
+                    <span>@Roxy Robin</span>
+                  </li>
+                </ul>
+              </div>
+              {/* fin reseaux */}
 
-          {/* debut autre */}
-          <div>
-            <h3
-              className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
-            >
-              autres
-            </h3>
-            <ul className="text-center flex flex-col gap-2 text-xl">
-              <li>Faq</li>
-              <li>Nos boutiques</li>
-              <li>Mentions légales</li>
-              <li>À propos de Foxy</li>
-              <li>Assistance</li>
-            </ul>
+              {/* debut autre */}
+              <div className="mb-20 xl:mb-0">
+                <h3
+                  className={`font-bold uppercase text-2xl xl:text-3xl mb-4 text-center ${chewy.className}`}
+                >
+                  autres
+                </h3>
+                <ul className="text-center flex flex-col gap-2 text-xl">
+                  <li>Faq</li>
+                  <li>Nos boutiques</li>
+                  <li>Mentions légales</li>
+                  <li>À propos de Foxy</li>
+                  <li>Assistance</li>
+                </ul>
+              </div>
+              {/* fin autre */}
+            </div>
           </div>
-          {/* fin autre */}
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
---
+++ app/components/wave/footer-svg.tsx
@@ -0,0 +1,15 @@
+export function FooterSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="mt-[-1px]"
+      viewBox="0 0 1440 320"
+    >
+      <path
+        fill="#f2f2f2"
+        fill-opacity="1"
+        d="M0,192L1440,320L1440,320L0,320Z"
+      ></path>
+    </svg>
+  );
+}
