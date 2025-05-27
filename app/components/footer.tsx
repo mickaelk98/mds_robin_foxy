4cbe96b7c1067d6e870820995d36251b60b67bfd
mickael keita <keitamickael@gmail.com>
2025-05-27 12:53:09 +0200

    ajout des liens des reseaux sociaux

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,6 +4,7 @@ import instagram from "@/app/assets/instagram.png";
 import facebook from "@/app/assets/facebook.png";
 import { Chewy } from "next/font/google";
 import { FooterSvg } from "@/app/components/wave/footer-svg";
+import Link from "next/link";
 
 const chewy = Chewy({
   subsets: ["latin"],
@@ -45,20 +46,30 @@ export function Footer() {
                 </h3>
                 <ul className="flex gap-5 md:flex-col items-center justify-center">
                   <li className="flex flex-col md:flex-row items-center gap-1">
-                    <Image
-                      src={instagram}
-                      alt="instagram"
-                      className="cursor-pointer"
-                    />
-                    <span>@foxy_robin_</span>
+                    <Link
+                      href="https://www.instagram.com/foxy_robin_/profilecard/"
+                      target="_blank"
+                    >
+                      <Image
+                        src={instagram}
+                        alt="instagram"
+                        className="cursor-pointer"
+                      />
+                      <span>@foxy_robin_</span>
+                    </Link>
                   </li>
                   <li className="flex flex-col md:flex-row items-center  gap-2">
-                    <Image
-                      src={facebook}
-                      alt="facebook"
-                      className="cursor-pointer"
-                    />
-                    <span>@Roxy Robin</span>
+                    <Link
+                      target="_blank"
+                      href="https://www.facebook.com/people/Roxy-Robin/61576403632725/?mibextid=wwXIfr&rdid=88nPvNCucPJvpGs0&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18qCvs91Go%2F%3Fmibextid%3DwwXIfr"
+                    >
+                      <Image
+                        src={facebook}
+                        alt="facebook"
+                        className="cursor-pointer"
+                      />
+                      <span>@Roxy Robin</span>
+                    </Link>
                   </li>
                 </ul>
               </div>
