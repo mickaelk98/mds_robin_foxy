4d04a7baeab84861fbed9ea196c2c04cebbbc31e
mickael keita <keitamickael@gmail.com>
2025-06-04 06:44:01 +0200

    ajout des liens dans la navigation et control de l'acces au page selon l'utilisateur, si il est authentifier ou non

--- app/components/desktop-header.tsx
+++ app/components/desktop-header.tsx
@@ -1,38 +1,40 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/app/assets/logo.png";
 import { Chewy } from "next/font/google";
-// import { File } from "@/app/components/header-items/file";
-// import { Forums } from "@/app/components/header-items/forums";
-// import { Faq } from "@/app/components/header-items/faq";
-// import { Login } from "@/app/components/header-items/login";
-// import { Signup } from "@/app/components/header-items/signup";
-// import { Shops } from "@/app/components/header-items/shops";
+import Link from "next/link";
+import { useAuthStore } from "@/app/lib/stores/auth-store";
+
 
 const chewy = Chewy({ subsets: ["latin"], weight: ["400"] });
 
 export function DesktopHeader() {
+  const { user } = useAuthStore();
+
+
   return (
     <header className="w-full bg-[var(--green-200)] lg:py-12">
       <div className="hidden w-full max-w-7xl mx-auto px-2 lg:flex lg:items-center lg:justify-between">
-        <Image src={logo} alt="logo" className="w-[200px] h-[100px]" />
+        <Link href="/"><Image src={logo} alt="logo" width={200} height={50} /></Link>
         <ul className={`flex gap-10 text-2xl uppercase ${chewy.className}`}>
+          {user && <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            <Link href="/files">Fiches</Link>
+          </li>}
+          {user && <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            <Link href="/forum">Forums</Link>
+          </li>}
           <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Fiches
+            <Link href="/faq">FAQ</Link>
           </li>
+          {!user && <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            <Link href="/login"> Connexion</Link>
+          </li>}
+          {!user && <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
+            <Link href="/signup">Inscription</Link>
+          </li>}
           <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Forum
-          </li>
-          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Faq
-          </li>
-          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Connexion
-          </li>
-          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Inscription
-          </li>
-          <li className="cursor-pointer border-b-4 border-[var(--green-200)] hover:border-[var(--orange-200)] hover:font-bold">
-            Boutiques
+            <Link href="shops">Boutiques</Link>
           </li>
         </ul>
       </div>
--- app/components/mobile-header-nav.tsx
+++ app/components/mobile-header-nav.tsx
@@ -1,19 +1,14 @@
 import { X } from "lucide-react";
 import { MobileNavItem } from "@/app/components/mobile-nav-item";
+import Link from "next/link";
+import { useAuthStore } from "@/app/lib/stores/auth-store";
 
 type Props = {
   closeNav: () => void;
 };
 
 export function MobileHeaderNav({ closeNav }: Props) {
-  const menu = [
-    "fiches",
-    "forum",
-    "faq",
-    "inscription",
-    "connexion",
-    "boutiques",
-  ];
+  const { user } = useAuthStore();
 
   return (
     <div className="fixed top-0 left-0 bg-[var(--orange-500)] w-full h-screen z-100 lg:hidden">
@@ -25,11 +20,12 @@ export function MobileHeaderNav({ closeNav }: Props) {
         />
       </div>
       <ul className="w-full h-full flex flex-col items-center justify-center">
-        {menu.map((item, index) => (
-          <li key={index}>
-            <MobileNavItem>{item}</MobileNavItem>
-          </li>
-        ))}
+        {user && <li><Link href="/files"><MobileNavItem>fiches</MobileNavItem></Link></li>}
+        {user && <li><Link href="/forum"><MobileNavItem>forum</MobileNavItem></Link></li>}
+        <li><Link href="/faq"><MobileNavItem>faq</MobileNavItem></Link></li>
+        {!user && <li><Link href="/signup"><MobileNavItem>inscription</MobileNavItem></Link></li>}
+        {!user && <li><Link href="/login"><MobileNavItem>connexion</MobileNavItem></Link></li>}
+        <li><Link href="/shops"><MobileNavItem>boutiques</MobileNavItem></Link></li>
       </ul>
     </div>
   );
