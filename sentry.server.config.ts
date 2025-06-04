116dfd35a0ca60dd9e7e9a346c26458a699527de
mickael keita <keitamickael@gmail.com>
2025-06-04 12:48:46 +0200

    ajout de sentry pour le suivi des logs d'erreurs

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fe69f3103690498c664de13d06f31735@o4509439775473664.ingest.de.sentry.io/4509439844483152",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
